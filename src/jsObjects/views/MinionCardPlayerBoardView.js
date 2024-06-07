8a02bb0f23a22b9ac70c5b8cb5236560fcce15c2
loliaravioli <marshmallowauthority@gmail.com>
2024-06-07 00:11:26 -0400

    reverting folder name because github isnt updating it for some reason

--- src/jsObjects/views/MinionCardPlayerBoardView.js
+++ src/jsObjects/views/MinionCardPlayerBoardView.js
@@ -31,7 +31,7 @@ export class MinionCardPlayerBoardView extends AbstractMinionBoardView {
         cardDiv.appendChild(healthValueBackground);
         healthValueBackground.appendChild(healthValue);
 
-        cardDiv.style.backgroundImage = "url('src/media/images/cardImages/" + this.card.constructor.name + ".jpg')";
+        cardDiv.style.backgroundImage = "url('src/media/images/cardimages/" + this.card.constructor.name + ".jpg')";
 
         return cardDiv;
     }
