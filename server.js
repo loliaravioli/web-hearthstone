d1f77b092c778afba5dd8c07d3b7007a52deecdd
loliaravioli <marshmallowauthority@gmail.com>
2024-06-09 20:06:00 -0400

    server infrastructure

--- server.js
+++ server.js
@@ -1,66 +1,123 @@
-/* SSL/non-SSL example with async/await functions */
+const { App } = require('uWebSockets.js'), /* or require('../dist/uws.js') ? */
+    { Server } = require('socket.io'),
+    { Pool } = require('pg'),
+    app = new App(),
+    io = new Server(),
+    PORT = 5500,
+    KEYS = {
+        REMAINING_LETTERS: 'remaining_letters',
+        PLAYER_COUNT: 'player_count',
+        BOARD_STATE: 'board_state',
+        WHOSE_TURN: 'whose_turn',
+        ROOM_CODE: 'room_code',
+        IP1: 'ip1',
+        IP2: 'ip2',
+        IP3: 'ip3',
+        IP4: 'ip4',
+        PASS1: 'pass1',
+        PASS2: 'pass2',
+        PASS3: 'pass3',
+        PASS4: 'pass4',
+        POINTS1: 'points1',
+        POINTS2: 'points2',
+        POINTS3: 'points3',
+        POINTS4: 'points4',
+        DOCK1: 'dock1',
+        DOCK2: 'dock2',
+        DOCK3: 'dock3',
+        DOCK4: 'dock4',
+        LAST_MODIFIED: 'last_modified'
+    }
 
-async function processMessage(text, ws) {
-    // Simulating an asynchronous operation (e.g., database call, file I/O)
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            ws.send(text);
-            console.log('Echoed message back to client:', text);
-            resolve();
-        }, 1 * 1000); // 1 second delay for demonstration
+io.attachApp(app);
+
+const dbConfig = process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 10000
+} : {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'scrabble',
+    password: 'admin',
+    port: 5432,
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 10000
+};
+const pool = new Pool(dbConfig);
+
+const queryHandlerInstance = new QueryHandler('game', pool);
+
+io.on("connection", (socket) => {
+    // associate the socket with the client's unique identifier
+    // this clientID is stored in the database to track who's playing in a scrabble game
+    const clientID = socket.handshake.query.clientID;
+    connectedClients[clientID] = socket;
+
+    debugConsole(CATEGORY.CONNECTED, clientID);
+
+    socket.on('disconnect', () => {
+        debugConsole(CATEGORY.DISCONNECTED, clientID);
+        delete connectedClients[socket.id];
+    });
+
+    socket.on('doFunction', (data) => {
+        const { var1, var2 } = data;
+        doFunction(socket, clientID, var1, var2);
+    });
+});
+
+async function doFunction(socket, clientID, var1, var2) {
+    const signature = arguments.callee.name;
+    console.log(signature);
+
+    try {
+        const record = await getRecord(id);
+
+        const scoreboard = [
+            [record[KEYS.IP1], record[KEYS.POINTS1]],
+            [record[KEYS.IP2], record[KEYS.POINTS2]],
+            [record[KEYS.IP3], record[KEYS.POINTS3]],
+            [record[KEYS.IP4], record[KEYS.POINTS4]]
+        ];
+
+        const whoseTurn = record[`ip${record[KEYS.WHOSE_TURN]}`];
+
+        socketEmit(socket, signature, true, { /* data */ });
+    } catch (err) {
+        console.error(err);
+        socketEmit(socket, signature, false, { /* data */ });
+    }
+}
+
+function socketEmit(socket, signature, success, data) {
+    socket.emit(`${signature}Response`, {
+        success: success,
+        signature: signature,
+        data: data
     });
 }
 
-const uWS = require('uWebSockets.js'), /* or require('../dist/uws.js') ? */
-    app = uWS.App(),
-    PORT = 9001;
+async function getRecord(id) {
+    const signature = arguments.callee.name;
+    console.log(signature);
 
-app./*SSL*/App({
-    key_file_name: 'misc/key.pem',
-    cert_file_name: 'misc/cert.pem',
-    passphrase: '1234'
-}).ws('/*', {
-    open: (ws) => {
-        console.log('A WebSocket connection has been opened!');
-    },
+    try {
+        const params = { where: [{ key: KEYS.ROOM_CODE, value: id }] };
+        const result = await queryHandlerInstance.select(params);
 
-    message: async (ws, message, isBinary) => {
-        const receivedText = new TextDecoder().decode(message);
-        console.log('Received message:', receivedText);
-
-        // Asynchronously echo the message back to the client
-        await processMessage(receivedText, ws);
-    },
-
-    close: (ws, code, message) => {
-        console.log('A WebSocket connection has been closed');
+        debugConsole(CATEGORY.SUCCESS, signature);
+        // query only ever returns one row since id is unique
+        return result[0];
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
-}).listen(PORT, (token) => {
-    if (token) {
-        console.log('Listening to port ' + PORT);
-    } else {
-        console.log('Failed to listen to port ' + PORT);
+}
+
+app.listen(PORT, (token) => {
+    if (!token) {
+        console.warn("port already in use");
     }
 });
-
-
-
-// TODO: try using this instead of express?
-// try using C++ backend to have better performance than node.js
-// const { App } = require('uWebSockets.js');
-// const { Server } = require('socket.io');
-
-// const app = new App();
-// const io = new Server();
-
-// io.attachApp(app);
-
-// io.on("connection", (socket) => {
-//     // ...
-// });
-
-// app.listen(3000, (token) => {
-//     if (!token) {
-//         console.warn("port already in use");
-//     }
-// });
