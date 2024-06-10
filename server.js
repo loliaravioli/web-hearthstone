8b2a6dad85801bdfb72b7f5c3a11d47fea9ab8c1
loliaravioli <marshmallowauthority@gmail.com>
2024-06-09 20:33:28 -0400

    working on server

--- server.js
+++ server.js
@@ -4,29 +4,29 @@ const { App } = require('uWebSockets.js'), /* or require('../dist/uws.js') ? */
     app = new App(),
     io = new Server(),
     PORT = 5500,
-    KEYS = {
-        REMAINING_LETTERS: 'remaining_letters',
-        PLAYER_COUNT: 'player_count',
-        BOARD_STATE: 'board_state',
-        WHOSE_TURN: 'whose_turn',
-        ROOM_CODE: 'room_code',
-        IP1: 'ip1',
-        IP2: 'ip2',
-        IP3: 'ip3',
-        IP4: 'ip4',
-        PASS1: 'pass1',
-        PASS2: 'pass2',
-        PASS3: 'pass3',
-        PASS4: 'pass4',
-        POINTS1: 'points1',
-        POINTS2: 'points2',
-        POINTS3: 'points3',
-        POINTS4: 'points4',
-        DOCK1: 'dock1',
-        DOCK2: 'dock2',
-        DOCK3: 'dock3',
-        DOCK4: 'dock4',
-        LAST_MODIFIED: 'last_modified'
+    KEYS = { // SQL keys
+        // REMAINING_LETTERS: 'remaining_letters',
+        // PLAYER_COUNT: 'player_count',
+        // BOARD_STATE: 'board_state',
+        // WHOSE_TURN: 'whose_turn',
+        // ROOM_CODE: 'room_code',
+        // IP1: 'ip1',
+        // IP2: 'ip2',
+        // IP3: 'ip3',
+        // IP4: 'ip4',
+        // PASS1: 'pass1',
+        // PASS2: 'pass2',
+        // PASS3: 'pass3',
+        // PASS4: 'pass4',
+        // POINTS1: 'points1',
+        // POINTS2: 'points2',
+        // POINTS3: 'points3',
+        // POINTS4: 'points4',
+        // DOCK1: 'dock1',
+        // DOCK2: 'dock2',
+        // DOCK3: 'dock3',
+        // DOCK4: 'dock4',
+        // LAST_MODIFIED: 'last_modified'
     }
 
 io.attachApp(app);
@@ -49,7 +49,7 @@ const pool = new Pool(dbConfig);
 
 const queryHandlerInstance = new QueryHandler('game', pool);
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
     // associate the socket with the client's unique identifier
     // this clientID is stored in the database to track who's playing in a scrabble game
     const clientID = socket.handshake.query.clientID;
@@ -116,8 +116,4 @@ async function getRecord(id) {
     }
 }
 
-app.listen(PORT, (token) => {
-    if (!token) {
-        console.warn("port already in use");
-    }
-});
+app.listen(PORT, (token) => { console.log(`Listening on port ${PORT}`); });
